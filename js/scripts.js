6d02c10c9a8ec27bcda194436ce191e80e23222d
Lew <lewdavidson47@gmail.com>
2017-10-25 16:05:43 -0700

    add quest variable

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 var pigSentence = function(userPhrase) {
   var wordArray = userPhrase.split("");
-  var vowels = ["a", "e", "i", "o", "u"];
-  var specialChar = ["q"];
+  var vowels = ["a", "e", "i", "o", "u","A","E", "I", "O", "U"];
+  var specialChar = ["q", "Q"];
   var letter1 = userPhrase.charAt(0);
   var letter2 = userPhrase.charAt(1);
   var letter3 = userPhrase.charAt(2);
@@ -12,26 +12,26 @@ var pigSentence = function(userPhrase) {
       if (vowels[x] === wordArray[0]) {
           $("#result").text(userPhrase + "-way");
         } else if ((vowels[x] !== wordArray[0]) && (vowels[x] !== wordArray[1]) && (vowels[x] !== wordArray[2])) {
-      var newThing = theCut + letter1 + letter2 + letter3 + "ay";
-      return newThing
+          var newThing = theCut + letter1 + letter2 + letter3 + "ay";
+          return newThing
         }
       }
-
         for (var index = 0; x < specialChar.length; x ++ ) {
           if (specialChar[x] === wordArray[0]); {
-            debugger
-            return quest; }
+            var quest = 
+            return quest;
           }
+        }
   };
 
-$(document).ready(function() {
-  $("form#pig-form").submit(function(event) {
-    event.preventDefault();
-    var userPhrase = $("input#phrase").val();
-    var result = pigSentence(userPhrase);
-    $("#result").text(result);
-  });
-});
+    $(document).ready(function() {
+      $("form#pig-form").submit(function(event) {
+        event.preventDefault();
+        var userPhrase = $("input#phrase").val();
+        var result = pigSentence(userPhrase);
+        $("#result").text(result);
+      });
+    });
 
 
  /* userPhrase.charAt(0); */
