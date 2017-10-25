7cc4a311ebec6c12f039c2b68e91e05da9161314
Lew <lewdavidson47@gmail.com>
2017-10-25 11:45:37 -0700

    making progress on functions

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,16 @@
 var pigSentence = function(userPhrase) {
-  return false;
+  var wordArray = userPhrase.split("");
+  var vowels = ["a", "e", "i", "o", "u"];
+    debugger
+    for (var index = 0; index === wordArray.length >= 1 ; index ++ ) { for (var vindex = 0; vowel.length <= 5; vindex ++) {
+      debugger
+      if (wordArray[index] === vindex[index]) {
+        debugger
+          wordArray.join();
+          $("#result").text(wordArray + "-way")
+        }
+      }
+    }
 };
 
 $(document).ready(function() {
