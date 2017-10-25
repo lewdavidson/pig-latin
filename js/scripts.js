417b7f7cfb47bc051def93c98245219efa7b2c59
Lew <lewdavidson47@gmail.com>
2017-10-25 15:17:57 -0700

    add consonant method

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,17 @@ var pigSentence = function(userPhrase) {
 
     for (var x = 0; x < vowels.length; x ++ ) {
       if (vowels[x] === wordArray[0]) {
-          $("#result").text(userPhrase + "-way")
+          $("#result").text(userPhrase + "-way");
+        } else if ((vowels[x] !== wordArray[0]) && (vowels[x] !== wordArray[1]) && (vowels[x] !== wordArray[2])) {
+          var letter1 = userPhrase.charAt(0);
+          var letter2 = userPhrase.charAt(1);
+          var letter3 = userPhrase.charAt(2);
+          var theCut = userPhrase.substr(3);
+          var newThing = theCut + letter1 + letter2 + letter3 + "ay";
+          return newThing
         }
-    }
-};
+      }
+  };
 
 $(document).ready(function() {
   $("form#pig-form").submit(function(event) {
@@ -20,3 +27,11 @@ $(document).ready(function() {
 
 
  /* userPhrase.charAt(0); */
+
+ // for (var index = 0; x < consonants.length; x ++ ) {
+ //   if (consonants[index] === wordArray[0]) {
+ //     debugger
+ //   }
+ // }
+
+//  var consonant = userPhrase.substring(0,1);
